e739771b3fe73881e716164e37cce2e50e65d331
yanbing <yanbing@corp.netease.com>
2023-11-20 11:00:39 +0800

    修修补补

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
-import React, { useEffect } from "react";
+import { ClarityScript } from "./ClarityScript";
 
 export const metadata: Metadata = {
   title: "小鱼智能客服",
@@ -29,35 +29,13 @@ interface RootLayoutProps {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  useEffect(() => {
-    // 创建 script 元素
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://www.clarity.ms/tag/jt8dl9t2q5";
-
-    // 定义脚本内容
-    script.text = `(function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "jt8dl9t2q5");`;
-
-    // 将脚本添加到文档中
-    document.head.appendChild(script);
-
-    // 清理函数
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []); // 空依赖数组确保脚本仅加载一次
-
   return (
     <html lang="en">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <ClarityScript /> {/* 在此处引入监控组件 */}
       </head>
       <body>{children}</body>
     </html>
