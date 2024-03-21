c01aca91e6e24207b5420ce3c2e6737d58db6d59
yanbing <yanbing@corp.netease.com>
2024-03-21 15:59:36 +0800

    新增使用教程

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -363,7 +363,7 @@ const cn = {
   Store: {
     DefaultTopic: "新的聊天",
     BotHello:
-      "开始聊天前，请前往[设置](/#/settings)页填入你自己的 API 密钥。\n如果在之前的小鱼上填写过 API 密钥，会自动带过来无需填写。\n**不知道如何获取 OpenAI API 密钥？** \n您可以在这里购买：[https://s.yiios.com/](https://s.yiios.com/)\n如果需要回到旧版，请点击这里：[https://ai-old-o.yiios.com/](https://ai-old-o.yiios.com/)\n如果需要联系维护者或与其他用户交流：\n![联系维护者](https://cardocr-1251789346.cos.ap-guangzhou.myqcloud.com/wx_qr.png)",
+      "开始聊天前，请前往[设置](/#/settings)页填入你自己的 API 密钥。\n**不知道如何使用小鱼客服助手？** \n使用教程：[https://www.yiios.com/post/xiao-yu-jing-xiang-zhan-shi-yong-jiao-cheng/](https://www.yiios.com/post/xiao-yu-jing-xiang-zhan-shi-yong-jiao-cheng/)\n**不知道如何获取 OpenAI API 密钥？** \n您可以在这里购买：[https://s.yiios.com/](https://s.yiios.com/)\n如果需要回到旧版，请点击这里：[https://ai-old-o.yiios.com/](https://ai-old-o.yiios.com/)\n如果需要联系维护者或与其他用户交流：\n![联系维护者](https://cardocr-1251789346.cos.ap-guangzhou.myqcloud.com/wx_qr.png)",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
